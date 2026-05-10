85c5c6bd1d9898728f399975e1c22bb9a5537249
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-05-10 23:51:24 +0200

    feat: register gostovanje and saradnik schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,5 @@
 import blog from './blog'
+import gostovanje from './gostovanje'
+import saradnik from './saradnik'
 
-export const schemaTypes = [blog]
+export const schemaTypes = [blog, gostovanje, saradnik]
